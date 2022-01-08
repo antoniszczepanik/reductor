7eb03df387f01d322593692d4cc7a3bf758cb953
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2022-01-08 22:47:50 +0100

    Create Huffman codes table

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"container/heap"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/icza/bitio"
 )
 
 type Node struct {
 	value       byte
 	freq        int
 	Left, Right *Node
+	isLeaf      bool
 	// Used only to represent nodes in graphviz format
 	id int
 }
@@ -25,16 +30,22 @@ func NewNode(id int, val byte, freq int, l, r *Node) Node {
 	}
 }
 
-// Stringer implementation allows to dump graphviz plot in this case.
-func (n Node) String() string {
-	repr := fmt.Sprintf("%d[label=\"value=%d freq=%d\"]\n", n.id, n.value, n.freq)
+func (n Node) DumpGraphviz(w io.Writer) {
+	w.Write([]byte("Digraph g {\n"))
+	w.Write([]byte(n.getGraphviz()))
+	w.Write([]byte("}\n"))
+}
+
+// getGraphviz recursively gets graphviz representation of all nodes with root at n.
+func (n Node) getGraphviz() string {
+	repr := fmt.Sprintf("\t%d[label=\"value=%d freq=%d\"]\n", n.id, n.value, n.freq)
 	if n.Left != nil {
-		repr += fmt.Sprintf("%d -> %d\n", n.id, n.Left.id)
-		repr += n.Left.String()
+		repr += fmt.Sprintf("\t%d -> %d\n", n.id, n.Left.id)
+		repr += n.Left.getGraphviz()
 	}
 	if n.Right != nil {
-		repr += fmt.Sprintf("%d -> %d\n", n.id, n.Right.id)
-		repr += n.Right.String()
+		repr += fmt.Sprintf("\t%d -> %d\n", n.id, n.Right.id)
+		repr += n.Right.getGraphviz()
 	}
 	return repr
 }
@@ -69,11 +80,11 @@ func (pq *PriorityQueue) RemoveEmpty() PriorityQueue {
 // constructHuffmanTree creates a tree for values and returns root *Node.
 func constructHuffmanTree(values []Value) Node {
 	freqs := make(PriorityQueue, 256)
-	// Id counter for the nodes.
-	var i int
+	var i int // i is id counter.
+
 	// Copy values into corresponding spots.
 	for i = 0; i < len(freqs); i++ {
-		freqs[i].value, freqs[i].id = byte(i), i
+		freqs[i].value, freqs[i].id, freqs[i].isLeaf = byte(i), i, true
 	}
 	for _, v := range values {
 		if v.IsLiteral {
@@ -94,10 +105,48 @@ func constructHuffmanTree(values []Value) Node {
 	return heap.Pop(&freqs).(Node)
 }
 
-// encodeHuffmanTree converts a tree into a translation unit.
-// Then it encodes it into binary, and appends encoded values after that.
-func encodeHuffmanTree(root *Node, values []Value) []uint64 {
-	return []uint64{1, 2, 3}
+func createCodeTable(root *Node, prefix string) map[string]string {
+	codeTable := make(map[string]string)
+	lt := make(map[string]string)
+	rt := make(map[string]string)
+	if root.isLeaf {
+		codeTable[fmt.Sprintf("%08b", root.value)] = prefix
+		return codeTable
+	}
+	if root.Left != nil {
+		lt = createCodeTable(root.Left, prefix+"0")
+	}
+	if root.Right != nil {
+		rt = createCodeTable(root.Right, prefix+"1")
+	}
+	return mergeTables(lt, rt)
+}
+
+type BinaryWriter struct {
+	w *bitio.Writer
+}
+
+func NewBinaryWriter(writer io.Writer) BinaryWriter {
+	w := bitio.NewWriter(writer)
+	return BinaryWriter{
+		w: w,
+	}
+}
+
+func (bw *BinaryWriter) Write(codeTable map[string]string, values []Value) {
+	for _, v := range values {
+		if v.IsLiteral {
+			//bw.w.Write()
+		}
+	}
+	bw.w.Close()
+}
+
+func mergeTables(a, b map[string]string) map[string]string {
+	for k, v := range b {
+		a[k] = v
+	}
+	return a
 }
 
 func min(a, b int) int {
@@ -122,16 +171,14 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
 		os.Exit(1)
 	}
-	//fmt.Println("File contents:")
-	//fmt.Println(string(input))
-	//fmt.Println("Values represantation:")
 	values := BytesToValues(input, 6, 128, 512) // LZ77
-	//for _, v := range values {
-	//	fmt.Printf("%v", v)
-	//}
 	root := constructHuffmanTree(values)
-	fmt.Printf("%v", root)
+	//root.DumpGraphviz(os.Stdout)
+	codeTable := createCodeTable(&root, "")
+	//fmt.Printf("%+v\n", codeTable)
 
-	//root := constructHuffmanTree(values) // Create huffman tree out of values
-	//binary := encodeHuffmanTree(root, values) // Create a translation map, parse into binary.
+	b := &bytes.Buffer{} // Just some writer interface.
+	bw := NewBinaryWriter(b)
+	bw.Write(codeTable, values)
+	fmt.Printf("% x", b)
 }
